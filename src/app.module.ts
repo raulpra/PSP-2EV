3fe3a014af127ca0afd61afa2c0a5795f35f38e7
raulpra <raulprada@gmail.com>
2026-05-23 13:39:25 +0200

    Creación docker-compose para lanzar base de datos y configuración

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'nestuser',
+      password: 'nestpassword',
+      database: 'discord_db',
+      entities: [],
+      synchronize: true,
+      logging: ['query', 'error'],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
