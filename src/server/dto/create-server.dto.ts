52d3cab611b5bfc78d03d7bffc2562b9a41d807a
raulpra <raulprada@gmail.com>
2026-05-24 11:27:24 +0200

    Creación de lógica de Server

--- src/server/dto/create-server.dto.ts
+++ src/server/dto/create-server.dto.ts
@@ -1,1 +1,15 @@
-export class CreateServerDto {}
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateServerDto {
+  @IsString()
+  @IsNotEmpty({ message: 'El nombre del servidor es obligatorio' })
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsNumber()
+  @IsNotEmpty({ message: 'Se necesita el ID del creador (ownerId)' })
+  ownerId: number; 
+}
