2e7cb6581dd01e6aa726d95294bfa69092e01c25
raulpra <raulprada@gmail.com>
2026-05-24 19:23:43 +0200

    Creada documentación Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  // Configuración de Swagger para la documentación de la API
+  const config = new DocumentBuilder()
+    .setTitle('Discord API')
+    .setDescription('API REST para un clon de Discord')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
